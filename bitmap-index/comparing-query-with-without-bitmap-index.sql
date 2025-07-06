a537ed8104a71acd2a3ef5cc06228e83ab94a4f8
skiperkrut <olga-aog@rambler.ru>
2025-07-06 14:50:46 +0300

    theme 11

---
+++ bitmap-index/comparing-query-with-without-bitmap-index.sql
@@ -0,0 +1,18 @@
+﻿-- включаем простую метрику времени
+SET timing ON
+
+-- 1. Без индекса
+DROP INDEX idx_cust_is_active;
+SELECT /*+ gather_plan_statistics */ COUNT(*)
+FROM demo_customers
+WHERE is_active = 'Y';
+SELECT * FROM TABLE(dbms_xplan.display_cursor(NULL,NULL,'ALLSTATS LAST'));
+
+-- 2. С индексом
+CREATE BITMAP INDEX idx_cust_is_active
+  ON demo_customers(is_active);
+
+SELECT /*+ gather_plan_statistics */ COUNT(*)
+FROM demo_customers
+WHERE is_active = 'Y';
+SELECT * FROM TABLE(dbms_xplan.display_cursor(NULL,NULL,'ALLSTATS LAST'));
